7ff9fa9e96f64cb13e6e1ae48fa29cfa12132f6b
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-01-13 23:34:22 +0100

    test counter

--- src/test/scala/com/myway/pfpvolpeg/CounterTest.scala
+++ src/test/scala/com/myway/pfpvolpeg/CounterTest.scala
@@ -1,11 +1,24 @@
 package com.myway.pfpvolpeg
 
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class CounterTest  extends AnyFunSuite with Matchers {
+class CounterTest extends AnyFunSuite with Matchers {
 
-  test("read file") {
+  test("in memory counter") {
+    val counter: IO[Counter[IO]] = Counter.make[IO]
+    val next: IO[Int] = for {
+      k <- counter
+      _ <- k.incr
+      _ <- k.incr
+      out <- k.get
+    } yield out
+
+    val result = next.unsafeRunSync()
+
+    result shouldBe 2
 
   }
 
