40081cf43c4d03a303dba2a6dfdfda08b5435352
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-01-17 18:28:18 +0100

    spotless format

--- src/main/scala/com/myway/pfpvolpeg/shopping/program/Checkout.scala
+++ src/main/scala/com/myway/pfpvolpeg/shopping/program/Checkout.scala
@@ -1,18 +1,16 @@
 package com.myway.pfpvolpeg.shopping.program
 
+import cats.MonadThrow
 import cats.data.NonEmptyList
 import cats.syntax.flatMap._
 import cats.syntax.functor._
-import cats.{Monad, MonadThrow}
 import com.myway.pfpvolpeg.shopping.domain._
-import cats.effect.IO
-import cats.effect.unsafe.implicits.global
 
 final case class Checkout[F[_] : MonadThrow](
-                                         payments: PaymentClient[F],
-                                         cart: ShoppingCart[F],
-                                         orders: Orders[F]
-                                       ) {
+                                              payments: PaymentClient[F],
+                                              cart: ShoppingCart[F],
+                                              orders: Orders[F]
+                                            ) {
   private def ensureNonEmpty[A](xs: List[A]): F[NonEmptyList[A]] =
     MonadThrow[F].fromOption(
       NonEmptyList.fromList(xs),
