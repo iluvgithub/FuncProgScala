907ae56ea27ea188904fdaa8b3d19a7b9b91f967
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-01-18 14:57:32 +0100

    reformatted

--- src/main/scala/com/myway/pfpvolpeg/shopping/program/Checkout.scala
+++ src/main/scala/com/myway/pfpvolpeg/shopping/program/Checkout.scala
@@ -6,11 +6,11 @@ import cats.syntax.flatMap._
 import cats.syntax.functor._
 import com.myway.pfpvolpeg.shopping.domain._
 
-final case class Checkout[F[_] : MonadThrow](
-                                              payments: PaymentClient[F],
-                                              cart: ShoppingCart[F],
-                                              orders: Orders[F]
-                                            ) {
+final case class Checkout[F[_]: MonadThrow](
+  payments: PaymentClient[F],
+  cart: ShoppingCart[F],
+  orders: Orders[F]
+) {
   private def ensureNonEmpty[A](xs: List[A]): F[NonEmptyList[A]] =
     MonadThrow[F].fromOption(
       NonEmptyList.fromList(xs),
@@ -19,11 +19,11 @@ final case class Checkout[F[_] : MonadThrow](
 
   def process(userId: UserId, card: Card): F[OrderId] =
     for {
-      c <- cart.get(userId)
+      c   <- cart.get(userId)
       its <- ensureNonEmpty(c.items)
       pid <- payments.process(Payment(userId, c.total, card))
       oid <- orders.create(userId, pid, its, c.total)
-      _ <- cart.delete(userId)
+      _   <- cart.delete(userId)
     } yield oid
 
 }
