cb177658e40aa2320ae8fcd639e7b9c0382e1aea
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-01-30 22:19:19 +0100

    removed

--- src/main/scala/com/myway/pfpvolpeg/shopping/domain/Category.scala
+++
@@ -1,8 +0,0 @@
-package com.myway.pfpvolpeg.shopping.domain
-
-import java.util.UUID
-
-final case class CategoryId(value: UUID)
-final case class CategoryName(name: String)
-
-final case class Category(categoryId: CategoryId, categoryName: CategoryName)
