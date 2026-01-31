e646aeef449e5bfca8856a97c76bc9308d37dcef
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-01-31 23:17:10 +0100

    config + for ... yield

--- src/main/scala/com/myway/sampleapp/main/HelloWorldHttpServer.scala
+++ src/main/scala/com/myway/sampleapp/main/HelloWorldHttpServer.scala
@@ -9,13 +9,11 @@ import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 import scala.concurrent.duration._
-
 // Swagger: https://chatgpt.com/c/69791f56-1ec4-838e-b118-963d25004c79
 object HelloWorldHttpServer extends IOApp {
 
   implicit val logger: Logger[IO] = Slf4jLogger.getLogger[IO]
 
-  // Server resource
   def server(httpConfig: HttpServerConfig): Resource[IO, Server] =
     EmberServerBuilder
       .default[IO]
@@ -25,16 +23,15 @@ object HelloWorldHttpServer extends IOApp {
       .withShutdownTimeout(5.seconds)
       .build
 
-  override def run(args: List[String]): IO[ExitCode] =
-    for {
-      httpServerConfig <- IO(
-        HttpServerConfig(
-          com.comcast.ip4s.Hostname.fromString("0.0.0.0").get,
-          com.comcast.ip4s.Port.fromInt(8080).get
-        )
+  override def run(args: List[String]): IO[ExitCode] = for {
+    httpConfig <- IO(
+      HttpServerConfig(
+        com.comcast.ip4s.Hostname.fromString("0.0.0.0").get,
+        com.comcast.ip4s.Port.fromInt(8080).get
       )
-      _ <- server(httpServerConfig)
-        .use(_ => logger.info(s"HTTP server started on $httpServerConfig") >> IO.never)
-    } yield ExitCode.Success
+    )
+    _ <- server(httpConfig)
+      .use(_ => logger.info("HTTP server started on http://localhost:8080") >> IO.never)
+  } yield ExitCode.Success
 
 }
