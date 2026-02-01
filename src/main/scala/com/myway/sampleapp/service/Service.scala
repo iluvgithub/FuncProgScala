270803ffca1e0586e3258eb52b4328d5b58e44d6
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-02-01 19:04:47 +0100

    mock implementation

--- src/main/scala/com/myway/sampleapp/service/Service.scala
+++ src/main/scala/com/myway/sampleapp/service/Service.scala
@@ -3,14 +3,20 @@ package com.myway.sampleapp.service
 import cats.effect.Async
 import org.typelevel.log4cats.Logger
 
-trait Service [F[_] ] {
+trait Service[F[_]] {
 
-  def sampleService(arg:String):F[String]
+  def sampleService(arg: String): F[String]
+
+  def redisWrite(key: String, value: String): F[Unit]
+  def redisRead(key: String): F[String]
 }
 
-case class SampleService[F[_]: Async: Logger]() extends Service [F]{
+case class SampleService[F[_]: Async: Logger]() extends Service[F] {
 
   override def sampleService(arg: String): F[String] =
     Async[F].pure(arg.toUpperCase)
 
+  override def redisWrite(key: String, value: String): F[Unit] = Async[F].pure(())
+
+  override def redisRead(key: String): F[String] = Async[F].pure("redis read")
 }
