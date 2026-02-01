1cb05d77c1596128d07ef4c2893dbee8587a19a3
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-02-01 22:06:14 +0100

    removed unused import

--- src/main/scala/com/myway/sampleapp/redis/RedisWriter.scala
+++ src/main/scala/com/myway/sampleapp/redis/RedisWriter.scala
@@ -2,7 +2,6 @@ package com.myway.sampleapp.redis
 import cats.effect._
 import cats.syntax.all._
 import com.myway.sampleapp.config.{AppConfigLoader, RedisConfig}
-import dev.profunktor.redis4cats.RedisCommands
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 object RedisWriter extends IOApp.Simple {
