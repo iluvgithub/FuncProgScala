750fd57664d4ea332dd776a0f30b7256ee2e3093
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-02-01 14:39:13 +0100

    added exit procedure

--- src/main/scala/com/myway/sampleapp/main/HelloWorldHttpServer.scala
+++ src/main/scala/com/myway/sampleapp/main/HelloWorldHttpServer.scala
@@ -5,6 +5,8 @@ import com.myway.sampleapp.config.{AppConfigLoader, HttpServerConfig}
 import com.myway.sampleapp.routes.HttpRoute
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.server.Server
+import org.slf4j
+import org.slf4j.LoggerFactory
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 
@@ -12,7 +14,8 @@ import scala.concurrent.duration._
 // Swagger: https://chatgpt.com/c/69791f56-1ec4-838e-b118-963d25004c79
 object HelloWorldHttpServer extends IOApp {
 
-  implicit val logger: Logger[IO] = Slf4jLogger.getLogger[IO]
+  implicit val logger: Logger[IO]   = Slf4jLogger.getLogger[IO]
+  implicit val logErr: slf4j.Logger = LoggerFactory.getLogger(getClass)
 
   def server(httpConfig: HttpServerConfig): Resource[IO, Server] =
     EmberServerBuilder
@@ -23,10 +26,15 @@ object HelloWorldHttpServer extends IOApp {
       .withShutdownTimeout(5.seconds)
       .build
 
-  override def run(args: List[String]): IO[ExitCode] = for {
+  override def run(args: List[String]): IO[ExitCode] = (for {
     appConfig <- AppConfigLoader.loadConfig[IO]()
     httpCfg   <- appConfig.getHttpServerConfig[IO]
-    _ <- server(httpCfg).use(_ => logger.info(s"HTTP server started on ${appConfig}") >> IO.never)
-  } yield ExitCode.Success
+    _ <- server(httpCfg).use(_ => logger.info(s"HTTP server started on $appConfig") >> IO.never)
+  } yield ExitCode.Success).handleErrorWith(switchToError)
+
+  private def switchToError(t: Throwable) = {
+    logErr.error("Failed to start Server", t)
+    IO(ExitCode.Error)
+  }
 
 }
