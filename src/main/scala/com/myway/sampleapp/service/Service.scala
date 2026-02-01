da7e30cd4d0471a83e3fbf27a552cb6189a84bfe
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-02-01 17:53:02 +0100

    added a sample service

--- src/main/scala/com/myway/sampleapp/service/Service.scala
+++ src/main/scala/com/myway/sampleapp/service/Service.scala
@@ -3,10 +3,14 @@ package com.myway.sampleapp.service
 import cats.effect.Async
 import org.typelevel.log4cats.Logger
 
-trait Service [F[_] ]{
+trait Service [F[_] ] {
 
+  def sampleService(arg:String):F[String]
 }
 
 case class SampleService[F[_]: Async: Logger]() extends Service [F]{
+
+  override def sampleService(arg: String): F[String] =
+    Async[F].pure(arg.toUpperCase)
 
 }
