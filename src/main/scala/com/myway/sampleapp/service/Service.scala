eb6fdb7aeeeffd838f7e984d0d0f952ea32ef82e
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-02-01 20:59:09 +0100

    redis introduced

--- src/main/scala/com/myway/sampleapp/service/Service.scala
+++ src/main/scala/com/myway/sampleapp/service/Service.scala
@@ -1,6 +1,8 @@
 package com.myway.sampleapp.service
 
 import cats.effect.Async
+import cats.syntax.all._
+import com.myway.sampleapp.redis.RedisReadWrite
 import org.typelevel.log4cats.Logger
 
 trait Service[F[_]] {
@@ -8,15 +10,19 @@ trait Service[F[_]] {
   def sampleService(arg: String): F[String]
 
   def redisWrite(key: String, value: String): F[Unit]
-  def redisRead(key: String): F[String]
+  def redisRead(key: String): F[Option[String]]
 }
 
-case class SampleService[F[_]: Async: Logger]() extends Service[F] {
+case class SampleService[F[_]: Async: Logger](
+  redisReadWrite: RedisReadWrite[F]
+) extends Service[F] {
 
   override def sampleService(arg: String): F[String] =
-    Async[F].pure(arg.toUpperCase)
+    Logger[F].info(s"Call to sample service: arg=$arg") *> Async[F].pure(arg.toUpperCase)
 
-  override def redisWrite(key: String, value: String): F[Unit] = Async[F].pure(())
+  override def redisWrite(key: String, value: String): F[Unit] =
+    redisReadWrite.write(key, value)
 
-  override def redisRead(key: String): F[String] = Async[F].pure("redis read")
+  override def redisRead(key: String): F[Option[String]] =
+    redisReadWrite.read(key)
 }
