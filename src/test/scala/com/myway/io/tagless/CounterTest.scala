e89ee24d01b04f4c19aefdf55a47b2e9762e85ac
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-02-22 23:32:57 +0100

    refactored program

--- src/test/scala/com/myway/io/tagless/CounterTest.scala
+++ src/test/scala/com/myway/io/tagless/CounterTest.scala
@@ -1,23 +1,34 @@
 package com.myway.io.tagless
 
+import cats.Monad
 import cats.effect.IO
+import cats.effect.std.Console
+import cats.implicits.{catsSyntaxApplicativeByValue, toFlatMapOps, toFunctorOps}
 import munit.CatsEffectSuite
 
 class CounterTest extends CatsEffectSuite {
 
+  case class CounterProgram[F[_]: Monad: Console](counter: Counter[F]) {
+    def program: F[Int] =
+      for {
+        _   <- display(counter)
+        _   <- counter.increment
+        _   <- display(counter)
+        _   <- repeat(5)(counter.increment)
+        out <- counter.get
+        _   <- display(counter)
+      } yield out
+    def display(counter: Counter[F]): F[Unit] =
+      counter.get.flatMap(Console[F].println)
+
+    def repeat(n: Int)(action: F[Unit]): F[Unit] =
+      action.replicateA(n).void
+  }
   test("increment and get on in memory counter") {
     // arrange
-    def program(c: Counter[IO]): IO[Int] =
-      for {
-        _   <- c.get.flatMap(IO.println)
-        _   <- c.increment
-        _   <- c.get.flatMap(IO.println)
-        _   <- c.increment.replicateA(5).void
-        out <- c.get
-        _   <- IO.println(out)
-      } yield out
+
     // act
-    val io: IO[Int] = Counter.make[IO].flatMap(program)
+    val io: IO[Int] = Counter.make[IO].flatMap(new CounterProgram[IO](_).program)
     // assert
 
     io.map { result =>
