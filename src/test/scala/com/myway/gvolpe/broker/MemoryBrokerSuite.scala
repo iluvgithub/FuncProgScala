9d2b132fa480f0df414aaac05bffe57e75fb1f0e
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-02-28 16:03:01 +0100

    added sleep

--- src/test/scala/com/myway/gvolpe/broker/MemoryBrokerSuite.scala
+++ src/test/scala/com/myway/gvolpe/broker/MemoryBrokerSuite.scala
@@ -4,6 +4,8 @@ import cats.effect.std.Queue
 import fs2.Stream
 import munit.CatsEffectSuite
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+
+import scala.concurrent.duration.DurationInt
 class MemoryBrokerSuite extends CatsEffectSuite {
 
   test(" 2 concurrent streams with foreground consumer and background producer") {
@@ -15,14 +17,14 @@ class MemoryBrokerSuite extends CatsEffectSuite {
       p = Stream
         .resource[IO, Producer[IO, Int]](producerResource)
         .flatMap(p => Stream.range(0, n).evalMap(i => p.send(i)))
-      c   = MemoryBroker.localConsumer[IO, Int](queue).receive
+      c   = MemoryBroker.localConsumer[IO, Int](queue).receive.metered(10.millis)
       out = c.concurrently(p)
     } yield out
 
     // act
     val ioList: IO[List[Int]] = io.flatMap(_.compile.toList)
     // assert
-    ioList.map(act => act shouldBe List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9))
+    ioList.map(act => act shouldBe List.range(0, n))
   }
 
 }
